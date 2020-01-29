8a816ec4ecffeefbec325d6ec975c9edfa2397cb
NicoTaborda <42037379+niico786@users.noreply.github.com>
2020-01-28 21:02:43 -0300

    Modificado clienteServicets para recibir datos desde el backend

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,25 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente.js';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http'
 import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ClienteService {
 
-private urlEndPoint : string = 'htpp://localhost:8080/api/clientes';
+private urlEndPoint:string = 'http://localhost:8080/api/clientes';
 
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    // return of(CLIENTES);
-    return this.http.get<Cliente[]>(this.urlEndPoint);
-    // return this.http.get(this.urlEndPoint).pipe(
-    //   map( response => response as Cliente[])
-    // );
+    return this.http.get(this.urlEndPoint).pipe(
+      map( (response) => response as Cliente[] )
+    );
   }
 
 }
