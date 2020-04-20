1ab90a460d2e8a42fbb46edd65cb8bdf4d4dc252
NicoTaborda <42037379+niico786@users.noreply.github.com>
2020-04-20 11:06:40 -0300

    Modificacion en import de cliente.service

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente.js';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http'
