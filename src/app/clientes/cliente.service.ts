e35467653c874a1e6ae8c8e4535d042bbc806646
NicoTaborda <42037379+niico786@users.noreply.github.com>
2020-05-14 14:43:03 -0300

    Agregado control de errores en getById

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente.js';
-import { Observable } from 'rxjs';
+import { of, Observable, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class ClienteService {
@@ -10,7 +12,7 @@ export class ClienteService {
 private urlEndPoint:string = 'http://localhost:8080/api/clientes';
 private httpHeaders = new HttpHeaders({'Content-type': 'application/json'})
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getClientes(): Observable<Cliente[]> {
     return this.http.get(this.urlEndPoint).pipe(
@@ -23,7 +25,14 @@ private httpHeaders = new HttpHeaders({'Content-type': 'application/json'})
   }
 
   getCliente(id): Observable<Cliente> {
-    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`)
+    return this.http.get<Cliente>(`${this.urlEndPoint}/${id}`).pipe(
+      catchError(e => {
+        this.router.navigate(['/clientes']);
+        console.error(e.error.mensaje, 'error');
+        Swal.fire('Error al editar!', e.error.mensaje, 'error');
+        return throwError(e); 
+      })
+    )
   }
 
   update(cliente: Cliente): Observable<Cliente>{
