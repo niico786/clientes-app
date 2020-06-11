7f9d21a1903a10262ce7798cd15858ef7739379b
NicoTaborda <42037379+niico786@users.noreply.github.com>
2020-06-10 23:05:00 -0300

    Agregada ruta del paginador y observable para las paginas

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -4,6 +4,7 @@ import { CLIENTES } from './clientes.json';
 import { ClienteService } from './cliente.service';
 import Swal from 'sweetalert2'
 import { tap } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-clientes',
@@ -13,28 +14,36 @@ export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
 
-  constructor(private service: ClienteService) { }
+  constructor(private service: ClienteService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    let page = 0;
-    this.service.getClientes(page)
-    .pipe(
-      tap(response => {
+    this.activatedRoute.paramMap.subscribe(params => {
+      let page: number = +params.get('page');
+
+      if (!page) {
+        page = 0;
+      }
+
+      this.service.getClientes(page)
+        .pipe(
+          tap(response => {
             console.log('ClientesComponent: tap3');
             (response.content as Cliente[]).forEach(cliente => {
               console.log(cliente.nombre);
             });
-      })
-    ).subscribe(response => this.clientes = response.content as Cliente[]);
+          })
+        ).subscribe(response => this.clientes = response.content as Cliente[]);
+    });
   }
 
-  delete(cliente: Cliente): void{
+  delete(cliente: Cliente): void {
     const swalWithBootstrapButtons = Swal.mixin({
       customClass: {
         confirmButton: 'btn btn-success',
         cancelButton: 'btn btn-danger'
       },
-        buttonsStyling: false
+      buttonsStyling: false
     })
 
     swalWithBootstrapButtons.fire({
