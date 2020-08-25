db840f8fcc8aec4f1eb2c4301635b1ce67de6075
NicoTaborda <niico786@gmail.com>
2020-08-24 22:15:05 -0300

    Implementado seleccion y subida de imagen para el detalle del cliente

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -98,4 +98,19 @@ private httpHeaders = new HttpHeaders({'Content-type': 'application/json'})
     )
   }
 
+  subirFoto(archivo: File, id){
+    let formData = new FormData();
+    formData.append("archivo", archivo);
+    formData.append("id", id);
+
+    return this.http.post(`${this.urlEndPoint}/upload/`, formData).pipe(
+      map( (response: any) => response.cliente as Cliente),
+      catchError(e => {
+        console.error(e.error.mensaje);
+        Swal.fire(e.error.mensaje, e.error.error, 'error');
+        return throwError(e);
+      })
+    );
+  }
+
 }
--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'detalle-cliente',
@@ -11,6 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class DetalleComponent implements OnInit {
   cliente: Cliente;   
   titulo: string = "Detalle del cliente";
+  private fotoSeleccionada: File;
 
   constructor(private clienteService: ClienteService,
      private ActivatedRoute: ActivatedRoute) { }
@@ -23,6 +25,19 @@ export class DetalleComponent implements OnInit {
           this.cliente = cliente;
         });
       }
+    });
+  }
+
+  seleccionarFoto(event){
+    this.fotoSeleccionada = event.target.files[0];
+    console.log(this.fotoSeleccionada);
+  }
+
+  subiFoto(){
+    this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
+    .subscribe(cliente => {
+      this.cliente = cliente;
+      Swal.fire('La foto se ha subido completamente',`La foto se ha subido con exito: ${this.cliente.foto}`, 'success');
     });
   }
 
