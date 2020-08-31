f9dea6327fcbf276f34c08714c501a75df8f0299
NicoTaborda <niico786@gmail.com>
2020-08-31 20:42:15 -0300

    Agregado barra de progreso al cargar la imagen del cliente

--- src/app/clientes/detalle/detalle.component.ts
+++ src/app/clientes/detalle/detalle.component.ts
@@ -3,6 +3,7 @@ import { Cliente } from '../cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
+import { HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'detalle-cliente',
@@ -12,7 +13,8 @@ import Swal from 'sweetalert2';
 export class DetalleComponent implements OnInit {
   cliente: Cliente;   
   titulo: string = "Detalle del cliente";
-  private fotoSeleccionada: File;
+  fotoSeleccionada: File;
+  progreso:number = 0;
 
   constructor(private clienteService: ClienteService,
      private ActivatedRoute: ActivatedRoute) { }
@@ -30,6 +32,7 @@ export class DetalleComponent implements OnInit {
 
   seleccionarFoto(event){
     this.fotoSeleccionada = event.target.files[0];
+    this.progreso = 0;
     console.log(this.fotoSeleccionada);
     if(this.fotoSeleccionada.type.indexOf('image') < 0){
       Swal.fire('Error selecionar imagen: ', 'El archivo debe ser del tipoi imagen', 'error');
@@ -42,9 +45,16 @@ export class DetalleComponent implements OnInit {
       Swal.fire('Error Upload: ', 'Debe seleccionar una foto', 'error');
     } else {
         this.clienteService.subirFoto(this.fotoSeleccionada, this.cliente.id)
-      .subscribe(cliente => {
-        this.cliente = cliente;
-        Swal.fire('La foto se ha subido completamente',`La foto se ha subido con exito: ${this.cliente.foto}`, 'success');
+      .subscribe(event => {
+        if (event.type === HttpEventType.UploadProgress) {
+          this.progreso = Math.round((event.loaded/event.total)*100);
+        } else if (event.type === HttpEventType.Response) {
+          let response: any = event.body;
+          this.cliente = response.cliente as Cliente; 
+          Swal.fire('La foto se ha subido completamente',response.mensaje, 'success');
+        }
+       // this.cliente = cliente;
+       
       });
     }
   }
